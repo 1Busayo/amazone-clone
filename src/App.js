6c2267d67fbafca2af685a62b18f56cfa7a849f8
unknown <fanimokunbusayo@gmail.com>
2020-12-02 03:27:27 +0100

    Amazon header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-     <h1> Hello React Bootcamp , let build the Amazon Store </h1>
+    <div className="app">
+    
+     <Header />
+
+
+     {/*Home*/}
     </div>
   );
 }
