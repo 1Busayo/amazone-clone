78251adda1d8080d00163ffb0eded2357f84cfbe
unknown <fanimokunbusayo@gmail.com>
2020-12-02 02:33:19 +0100

    initial react template

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA9NW0p-d5cSuSGh4UvVM6o-S8fD0DMqeM",
+    authDomain: "clone-1b332.firebaseapp.com",
+    databaseURL: "https://clone-1b332.firebaseio.com",
+    projectId: "clone-1b332",
+    storageBucket: "clone-1b332.appspot.com",
+    messagingSenderId: "998486488152",
+    appId: "1:998486488152:web:292c70379a56d6d095c62d",
+    measurementId: "G-FN7HQQTTWM"
+  };
