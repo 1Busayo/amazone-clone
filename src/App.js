0d3aa106e089d8acea3273648c22e684c13f845e
unknown <fanimokunbusayo@gmail.com>
2020-12-04 00:13:48 +0100

    using react-route-dom

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React from "react";
 import './App.css';
 import Header from './Header';
 import Home from './Home';
-
 import {BrowserRouter as Router , Switch ,Route} from "react-router-dom";
+import Checkout from "./Checkout";
 function App() {
   return (
     <Router>
 
 <div className="app">
+  <Header />
      <Switch>
        <Route path="/checkout">
-     <Header />
-     <h1>I AM A CHECKOUT, SMASH THE LIKE BUTTON</h1>
+     
+     <Checkout />
       </Route>
+     
       <Route path="/">
-     <Header />
      
      <Home />
       </Route>
--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className="header">
+          <Link to='/'>
             <img
             className="header__logo"
             src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""/>
-        
-
+         </Link>
+               
 <div className="header__search">
 <input className="header__searchInput" type="text" />
 <SearchIcon className="header__searchIcon" />
@@ -47,14 +49,14 @@ function Header() {
       Prime
     </span>
 </div>
-
+<Link to='/checkout'>
 <div className="header__optionBasket">
     <ShoppingBasketIcon />
     <span className="header__optionLineTwo header__basketCount">
       0
     </span>
 </div>
-
+</Link>
 </div>
 
 </div>
