77a610426dee06b1e37b693664d2136e6644677d
unknown <fanimokunbusayo@gmail.com>
2020-12-05 21:03:41 +0100

    Deploy to firebase

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from './StateProvider';
 import CheckoutProduct from './CheckoutProduct';
 
 function Checkout() {
-const [{basket}, dispatch] = useStateValue();
+const [{basket , user}, dispatch] = useStateValue();
 
     return (
          <div className="checkout">
@@ -13,6 +13,7 @@ const [{basket}, dispatch] = useStateValue();
           <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
      
      <div>
+    <h3>{user?.email}</h3>
          <h2 className="checkout__title">
              Your shopping basket
          </h2>
